16c8be64c228c63614b2c03a0e042c530ff29714
yaser8541 <443017396@sm.imamu.edu.sa>
2026-04-21 15:17:39 +0300

    Network issue 2

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ process.on("unhandledRejection", (reason) => {
   console.error("[unhandledRejection]", reason);
 });
 
+app.use((req, res, next) => {
+  console.log(`[REQ] ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
